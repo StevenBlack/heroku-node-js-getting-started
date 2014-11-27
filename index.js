3acefb854c557753577da61aaa0f29d97df03cd5
Steven Black <steveb@stevenblack.com>
2014-11-26 23:40:49 -0500

    Another mod to the demo.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', function(request, response) {
   var result = ''
   var times = process.env.TIMES || 5
   for (i=0; i < times; i++)
-    result += cool();
+    result += cool() + " ";
   response.send(result);
 });
 
